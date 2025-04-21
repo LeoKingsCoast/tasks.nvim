95dadb42c985536f35e47a121041c159498c9c42
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-21 20:21:15 -0300

    Make command to write changes outside the window

--- plugin/load_tasks.lua
+++ plugin/load_tasks.lua
@@ -1,3 +1,7 @@
-vim.api.nvim_create_user_command("OpenTasks", function ()
+vim.api.nvim_create_user_command("TasksOpen", function ()
   require("tasks").open_tasks()
+end, {})
+
+vim.api.nvim_create_user_command("TasksWrite", function ()
+  require("tasks.core").write_state()
 end, {})
