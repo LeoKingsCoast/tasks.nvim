3ed0b90963a9dd7db45c37a7cf061142914f7215
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-21 20:07:09 -0300

    Improve user interface

--- lua/tasks.lua
+++ lua/tasks.lua
@@ -1,9 +1,25 @@
 local ui = require("tasks.ui")
-local search = require("tasks.search")
+local core = require("tasks.core")
 
-local open_tasks = function (opts)
-  local task_window = ui.open()
-  ui.fill(task_window)
+local M = {}
+
+---@param root_dir? string
+---@return Task[]
+M.get_tasks = function (root_dir)
+  -- Get a table with all tasks found in the given directory. Defaults to project
+  -- root directory if not specified
+  root_dir = root_dir or "."
+  return core.get_tasks(root_dir)
 end
 
-vim.api.nvim_create_user_command("OpenTasks", open_tasks, {})
+---@param root_dir? string
+M.open_tasks = function (root_dir)
+  -- Open a window and show all found tasks in the given directory. Defaults to
+  -- project root directory if not specified
+  root_dir = root_dir or "."
+  local task_window = ui.open()
+  ui._fill(task_window, root_dir)
+end
+
+return M
+
--- plugin/load_tasks.lua
+++ plugin/load_tasks.lua
@@ -1,1 +1,3 @@
-require("tasks")
+vim.api.nvim_create_user_command("OpenTasks", function ()
+  require("tasks").open_tasks()
+end, {})
