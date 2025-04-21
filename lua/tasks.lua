65785c37eb920a33e2dd7000cbc070b978713f76
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-21 10:15:12 -0300

    Implement user command and automatic loading

--- lua/tasks.lua
+++ lua/tasks.lua
@@ -2,14 +2,8 @@ local ui = require("tasks.ui")
 local search = require("tasks.search")
 
 local open_tasks = function (opts)
-  ui.create_floating_window()
-end
-
-local search_tasks = function (pattern)
-  search.search(pattern, ".")
+  local task_window = ui.open()
+  ui.fill(task_window)
 end
 
 vim.api.nvim_create_user_command("OpenTasks", open_tasks, {})
-vim.api.nvim_create_user_command("SearchTasks", function (args)
-  search_tasks(args.args)
-end, { nargs = 1 })
